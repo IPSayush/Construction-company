a6a2d8123f83aa02e04066711e4c8fd515695fde
ayush8627 <pnchalprakashan@gmail.com>
2025-09-19 15:28:22 +0530

    add more project section

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ bar_div.addEventListener("click" , (()=> {
 
 }));
 
+// questions to ask sir
+// agar hm containt likhne ke liye headings ka use na krke paragraph ka use kre or uska font size or width bda ke usse heading jaise dekhne layak banaye to usse koi problem to na hogi ?
+// wix ki website se discover section wala slider kaise banaye
+// mere project ka footer contaont design karao
+// how to make it responsive
+//
