ec49b600e9b75878ae80ffc2c4ae440b1a7cf8f5
ayush8627 <pnchalprakashan@gmail.com>
2025-09-22 09:56:52 +0530

    add some styling for mobile view hamverger and nav 2

--- script.js
+++ script.js
@@ -14,9 +14,41 @@ if (flag) {
         flag = true;
     }
 }))
+let links = nav_2.querySelectorAll("a");
+links.forEach((link)=>{
+    link.addEventListener("click",()=>{
+        nav_2.style.left = "100vw";
+        hamburger.style.color = "black"
+        hamburger.style.backgroundColor = "#f5f5f5";
+        flag = true;
+    })
+})
+
 // questions to ask sir
 // agar hm containt likhne ke liye headings ka use na krke paragraph ka use kre or uska font size or width bda ke usse heading jaise dekhne layak banaye to usse koi problem to na hogi ?
 // wix ki website se discover section wala slider kaise banaye
 // mere project ka footer contaont design karao
 // how to make it responsive
 //
+// This is for infinity scroll
+// const parent = document.getElementById('more_project_div_2');
+
+// // Duplicate child elements for seamless scroll
+// function duplicateChildren() {
+//     for (let i = 0; i < 4; i++) {
+//         parent.appendChild(parent.children[i].cloneNode(true));
+//     }
+// }
+// duplicateChildren();
+
+// const scrollWidth = parent.scrollWidth / 2;
+
+// parent.scrollLeft = scrollWidth; // Start from middle
+
+// parent.addEventListener('scroll', () => {
+//     if (parent.scrollLeft <= 0) {
+//         parent.scrollLeft = scrollWidth;
+//     } else if (parent.scrollLeft >= parent.scrollWidth - parent.clientWidth) {
+//         parent.scrollLeft = scrollWidth - parent.clientWidth;
+//     }
+// });
